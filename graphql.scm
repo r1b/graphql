49b397f21e27ebe0980c53c2e160092faa1d9a44
Robert C Jensen <robert.cole.jensen@gmail.com>
2019-03-26 20:52:42 -0400

    add note

--- graphql.scm
+++ graphql.scm
@@ -1,6 +1,8 @@
 (module graphql ()
   (import matchable scheme utf8)
 
+  ; Looking at graphql-js we need to make this a little smarter
+  ; See https://github.com/graphql/graphql-js/blob/8c96dc8276f2de27b8af9ffbd71a4597d483523f/src/language/lexer.js#L102-L125
   (define (tokenize chars)
     (match chars
       ((#\uFEFF tail ...) (cons 'UNICODE-BOM (tokenize tail)))
