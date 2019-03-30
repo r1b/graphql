dacab59f277a4b1cb2b14dc725eb50418fbf3ecd
Robert C Jensen <robert.cole.jensen@gmail.com>
2019-03-30 16:10:20 -0400

    s/tokenize/lex/g

--- lex.scm
+++ lex.scm
@@ -1,36 +1,36 @@
-(module lex (tokenize)
+(module lex (lex)
   (import (chicken base) matchable scheme utf8)
 
   (define (string-append-char s . cs)
     (apply string-append (cons s (map (cut make-string 1 <>) cs))))
 
-  (define (tokenize-block-string chars #!optional (value ""))
+  (define (lex-block-string chars #!optional (value ""))
     ; TODO dedent
     ; FIXME this is like wrong
     (match chars
-      ((#\" #\" #\" tail ...) (cons '(BLOCK-STRING value) (tokenize tail)))
+      ((#\" #\" #\" tail ...) (cons '(BLOCK-STRING value) (lex tail)))
       ((char tail ...)
-       (tokenize-block-string tail (string-append-char value char)))))
+       (lex-block-string tail (string-append-char value char)))))
 
-  (define (tokenize-comment chars)
+  (define (lex-comment chars)
     (match-lambda
       ((or (#\u000D #\u000A tail ...) ((or #\u000A #\u000D) tail ...))
-       (tokenize tail))
-      ((_ tail ...) (tokenize-comment tail))))
+       (lex tail))
+      ((_ tail ...) (lex-comment tail))))
 
-  (define (tokenize-name chars #!optional (value ""))
+  (define (lex-name chars #!optional (value ""))
     (match chars
       (((and char (or (? char-alphabetic?) (? char-numeric?) #\_)) tail ...)
-       (tokenize-name tail (string-append-char value char)))
-      ((_ tail ...) (cons '(NAME value) (tokenize tail)))))
+       (lex-name tail (string-append-char value char)))
+      ((_ tail ...) (cons '(NAME value) (lex tail)))))
 
-  (define (tokenize-digits chars value)
+  (define (lex-digits chars value)
     ((compose
        (lambda (chars value)
          (match chars
            (((and digit (? char-numeric?)) tail ...)
-            (tokenize-digits tail (string-append-char tail digit)))
-           (_ (cons '(NUMBER (string->number value)) (tokenize chars)))))
+            (lex-digits tail (string-append-char tail digit)))
+           (_ (cons '(NUMBER (string->number value)) (lex chars)))))
 
        (lambda (chars value)
          (match chars
@@ -40,23 +40,23 @@
 
      chars value))
 
-  (define (tokenize-number chars)
+  (define (lex-number chars)
     ((compose
        (lambda (chars value)
          (match chars
            (((and sign (or #\+ #\-)) tail ...)
-            (tokenize-digits tail (string-append-char value sign))
-            (_ (tokenize-digits chars value)))))
+            (lex-digits tail (string-append-char value sign))
+            (_ (lex-digits chars value)))))
 
        (lambda (chars value)
          (match chars
            (((and char (or #\E #\e)) tail ...)
             (values tail (string-append-char value char)))
-           (_ (tokenize-digits chars value))))
+           (_ (lex-digits chars value))))
 
        (lambda (chars value)
          (match chars
-           ((#\. tail ...) (tokenize-digits tail (string-append-char value #\.)))
+           ((#\. tail ...) (lex-digits tail (string-append-char value #\.)))
            (_ (values chars value))))
 
        (lambda (chars value)
@@ -66,7 +66,7 @@
                 (if (char-numeric? (car tail))
                     (error "Unexpected digit after 0" (car tail))
                     (values tail (string-append-char value digit)))
-                (tokenize-digits tail (string-append-char value digit))))
+                (lex-digits tail (string-append-char value digit))))
            (_ (values chars value))))
 
        (lambda (chars value)
@@ -76,37 +76,37 @@
 
      chars ""))
 
-  (define (tokenize-string chars)
+  (define (lex-string chars)
     )
 
   ; See https://github.com/graphql/graphql-js/blob/8c96dc8276f2de27b8af9ffbd71a4597d483523f/src/language/lexer.js#L102-L125
   ; TODO: Preserve line / character for errors
-  (define (tokenize chars)
+  (define (lex chars)
     (match-lambda
       ; ignored tokens
-      ((#\uFEFF tail ...) (tokenize tail))
-      (((or #\u0009 #\u0200) tail ...) (tokenize tail))
+      ((#\uFEFF tail ...) (lex tail))
+      (((or #\u0009 #\u0200) tail ...) (lex tail))
       ((or (#\u000D #\u000A tail ...) ((or #\u000A #\u000D) tail ...))
-       (tokenize tail))
+       (lex tail))
       ; the real deal
-      ((#\! tail ...) (cons '(BANG) (tokenize tail)))
-      ((#\# comment-tail ...) (tokenize-comment comment-tail))
-      ((#\$ tail ...) (cons '(DOLLAR) (tokenize tail)))
-      ((#\& tail ...) (cons '(AMP) (tokenize tail)))
-      ((#\( tail ...) (cons '(PAREN-L) (tokenize tail)))
-      ((#\) tail ...) (cons '(PAREN-R) (tokenize tail)))
+      ((#\! tail ...) (cons '(BANG) (lex tail)))
+      ((#\# comment-tail ...) (lex-comment comment-tail))
+      ((#\$ tail ...) (cons '(DOLLAR) (lex tail)))
+      ((#\& tail ...) (cons '(AMP) (lex tail)))
+      ((#\( tail ...) (cons '(PAREN-L) (lex tail)))
+      ((#\) tail ...) (cons '(PAREN-R) (lex tail)))
       ; FIXME handle dot followed by invalid char
-      ((#\. #\. #\. tail ...) (cons '(SPREAD) (tokenize tail)))
-      ((#\: tail ...) (cons '(COLON) (tokenize tail)))
-      ((#\= tail ...) (cons '(EQUALS) (tokenize tail)))
-      ((#\@ tail ...) (cons '(AT) (tokenize tail)))
-      ((#\[ tail ...) (cons '(BRACKET-L) (tokenize tail)))
-      ((#\] tail ...) (cons '(BRACKET-R) (tokenize tail)))
-      ((#\{ tail ...) (cons '(BRACE-L) (tokenize tail)))
-      ((#\| tail ...) (cons '(PIPE) (tokenize tail)))
-      ((#\} tail ...) (cons '(BRACE-R) (tokenize tail)))
-      (((? char-alphabetic?) tail ...) (tokenize-name chars))
-      (((or #\- (? char-numeric?)) tail ...) (tokenize-number chars))
-      ((#\" #\" #\" tail ...) (tokenize-block-string chars))
-      ((#\" tail ...) (tokenize-string tail))
+      ((#\. #\. #\. tail ...) (cons '(SPREAD) (lex tail)))
+      ((#\: tail ...) (cons '(COLON) (lex tail)))
+      ((#\= tail ...) (cons '(EQUALS) (lex tail)))
+      ((#\@ tail ...) (cons '(AT) (lex tail)))
+      ((#\[ tail ...) (cons '(BRACKET-L) (lex tail)))
+      ((#\] tail ...) (cons '(BRACKET-R) (lex tail)))
+      ((#\{ tail ...) (cons '(BRACE-L) (lex tail)))
+      ((#\| tail ...) (cons '(PIPE) (lex tail)))
+      ((#\} tail ...) (cons '(BRACE-R) (lex tail)))
+      (((? char-alphabetic?) tail ...) (lex-name chars))
+      (((or #\- (? char-numeric?)) tail ...) (lex-number chars))
+      ((#\" #\" #\" tail ...) (lex-block-string chars))
+      ((#\" tail ...) (lex-string tail))
       (((and bad-char _) tail ...) (error "Unexpected character" bad-char)))))
